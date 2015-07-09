340cf6d15b1276c65db8817c5d625c3e41158423
Robin Chauhan <robin.chauhan@outlook.com>
2015-07-10 01:13:16 +0530

    Chart integrated

---
+++ public/js/directives/line-chart-dir.js
@@ -0,0 +1,52 @@
+angular.module('stockApp_LineChartDRMD', [])
+  .directive('lineChartDir', [
+    '$compile',
+    '$timeout',
+
+    function ($compile, $timeout) {
+      return {
+        scope: {
+          config: "=config"
+        },
+        link: function (scope, element) {
+          element.highcharts({
+            chart: {
+              type: 'line'
+            },
+            title: {
+              text: "ABC Corporation Ltd."
+            },
+            xAxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            yAxis: {
+              title: {
+                text: 'Price'
+              }
+            },
+            tooltip: {
+              valueSuffix: '$'
+            },
+            plotOptions: {
+              line: {dataLabels: {
+                enabled: true,
+                formatter: function () {
+                  if (this.point.x % 2 == 0) return '';
+                  return this.y + '$';
+                }
+              }
+              }
+            },
+            series: [
+              {
+                name: 'Stock Price',
+                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+              }
+            ]
+          });
+
+          scope.chart = element.highcharts();
+
+        }
+      }
+    }]);
