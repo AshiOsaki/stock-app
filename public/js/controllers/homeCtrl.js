98c6ee60a84fd50c9d1f9141d83750f7fa1fa178
Robin Chauhan <robin.chauhan@outlook.com>
2015-07-12 01:37:40 +0530

    Progressive Changes

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -3,7 +3,7 @@ angular.module('stockAppHomeCtrlMD', []).
     '$scope',
     'CoreHttpSV',
     function ($scope, CoreHttpSV) {
-      $scope.dummySearchElement = [
+      $scope.stockList = [
         "ABC Company",
         "DEF Company",
         "ADC Company",
@@ -14,19 +14,55 @@ angular.module('stockAppHomeCtrlMD', []).
         "STO Company"
       ];
 
-      $scope.fetchStockDetails = function(stockName){
+      $scope.searchListArray = angular.copy($scope.stockList);
+
+      $scope.selectedStockName = "";
+      $scope.validStockSelected = false;
+      $scope.requestedEndDate = new Date();
+      $scope.hideSearchList = false;
+
+      $scope.fetchStockDetails = function (stockName) {
         var _config = {
           url: '',
           method: 'GET'
         };
         CoreSV.httpSV(_config).then(function (data) {
           console.log(data);
-        },function (error) {
+        }, function (error) {
           console.log('Error while adding customer: ', error);
           $scope.$emit('processIndicator', false);
         })
-      }
+      };
 
+      $scope.setStockName = function () {
+        $scope.selectedStockName = event.currentTarget.text;
+        $scope.validStockSelected = true;
+      };
+
+      $scope.getQuote = function () {
+        if ($scope.selectedStockName.length > 3 && $scope.validStockSelected) {
+          $scope.hideSearchList = true;
+        }
+      };
+
+      $scope.createSearchList = function () {
+        if ($scope.selectedStockName.length > 3) {
+          var _stockList = $scope.stockList,
+            _length = _stockList.length,
+            _stingToCheck = $scope.selectedStockName,
+            _newSearchArray = [];
+          for (var i = 0; i < _length; i++) {
+            if (_stockList[i].substring(0, _stingToCheck.length) === _stingToCheck) {
+              _newSearchArray.push(_stockList[i]);
+            }
+          }
+          $scope.searchListArray = _newSearchArray;
+          $scope.hideSearchList = false;
+        }
+        else if ($scope.selectedStockName.length === 0) {
+          $scope.searchListArray = [];
+        }
+      }
 
 
     }]);
